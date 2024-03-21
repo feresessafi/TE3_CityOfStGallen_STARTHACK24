d154630ce80cf912d8c6c8593cef31d01f50ba9f
ge87nuq <ge87nuq@mytum.de>
2024-03-21 21:26:38 +0100

    safari erlaubt

--- frontend/pages/students.vue
+++ frontend/pages/students.vue
@@ -1,20 +1,16 @@
 <template>
   <div
-    class="dynamic-background flex flex-col items-center justify-center p-24"
+    class="min-h-screen flex flex-col items-center justify-center bg-gray-50 p-24"
   >
-    <h1 class="text-8xl font-bold text-blue-900 mb-20">Your Students</h1>
+    <h1 class="text-8xl font-bold text-gray-800 mb-20">Your Students</h1>
     <div class="flex flex-wrap justify-center gap-8">
       <button
         v-for="student in students"
         :key="student.firstname"
-        class="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-xl font-medium text-blue-900 rounded-lg group bg-gradient-to-br from-orange-200 via-blue-200 to-orange-200 group-hover:from-orange-300 group-hover:via-blue-300 group-hover:to-orange-300 hover:text-blue-900 focus:ring-4 focus:outline-none focus:ring-orange-200"
+        class="transition ease-in-out duration-150 flex justify-center py-2 px-4 border border-transparent rounded-md shadow text-xl font-medium text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         @click="showModal(student)"
       >
-        <span
-          class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-opacity-0"
-        >
-          {{ student.firstname }} {{ student.lastname }}
-        </span>
+        {{ student.firstname }} {{ student.lastname }}
       </button>
     </div>
     <!-- Use the modal component -->
@@ -61,24 +57,3 @@ export default {
   },
 };
 </script>
-
-<style>
-@keyframes backgroundAnimation {
-  0% {
-    background-position: 0% 50%;
-  }
-  50% {
-    background-position: 100% 50%;
-  }
-  100% {
-    background-position: 0% 50%;
-  }
-}
-
-.dynamic-background {
-  background: linear-gradient(270deg, #ffedd5e9, #dbeafee3);
-  background-size: 400% 400%;
-  animation: backgroundAnimation 30s ease infinite;
-  min-height: 100vh;
-}
-</style>
